7721f57779fb25bf75667417c2538098321ef803
Emjay Kim <emjay.kr@krustuniverse.com>
2021-10-03 22:09:03 +0900

    initial commit

---
+++ patch.js
@@ -0,0 +1,22 @@
+const { GanacheGasMultiplierProvider } = require("hardhat/internal/core/providers/gas-providers");
+var originalIsGanache = GanacheGasMultiplierProvider.prototype._isGanache
+GanacheGasMultiplierProvider.prototype._isGanache = async function() {
+    try {
+        return await originalIsGanache.apply(this)
+    } catch (error) {
+        if (error.message == 'The method web3_clientVersion does not exist/is not available') {
+            GanacheGasMultiplierProvider.prototype._cachedIsGanache = false
+            return false
+        } 
+        throw error
+    }
+}
+
+const { Formatter } = require("@ethersproject/providers")
+var orignialReceipt = Formatter.prototype.receipt
+Formatter.prototype.receipt = function(receipt) {
+    if (receipt.cumulativeGasUsed == null) {
+        receipt.cumulativeGasUsed = receipt.gasUsed
+    }
+    return orignialReceipt.apply(this, arguments)
+}
